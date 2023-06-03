562d3823d46fb76ee5f6d0868bb3f9ca68a20428
vignesh-seven <vignesh77024@gmail.com>
2023-06-03 17:44:33 +0530

    first commit

---
+++ sway/set-wallpaper.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wallpaper="/home/vin/.config/wallpapers/anime-city.jpg"
+
+# set the wallpaper
+swaymsg output "*" bg $wallpaper fill
+
+# generate the color scheme
+# wal -i $wallpaper
+
+exit
